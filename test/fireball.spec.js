dd6d42b0338ac3c15c26c02795556b960864b585
Dima Fitiskin <dfitiskin@gmail.com>
2017-06-02 15:14:31 +0400

    Исправил тесты для Fireball::act, добавив проверку вызова метода Level::obstacleAt

--- test/fireball.spec.js
+++ test/fireball.spec.js
@@ -104,24 +104,30 @@ describe('Класс Fireball', () => {
 
     it('Вызывает level.obstacleAt со своим вектором размера', () => {
       const ball = new Fireball(position, speed);
+      let isCalled = false;
       const level = {
         obstacleAt(pos, size) {
           expect(size).to.eql(new Vector(1, 1));
+          isCalled = true;
         }
       };
 
       ball.act(time, level);
+      expect(isCalled).to.be.true;
     });
 
     it('Вызывает level.obstacleAt с вектором новой позиции', () => {
       const ball = new Fireball(position, speed);
+      let isCalled = false;
       const level = {
         obstacleAt(pos, size) {
           expect(pos).to.eql(new Vector(10, 5));
+          isCalled = true;
         }
       };
 
       ball.act(time, level);
+      expect(isCalled).to.be.true;
     });
   });
 });
